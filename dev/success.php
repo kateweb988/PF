3dc4cfc0bb4d2931fcd423a354cd9185d037d414
kateweb988 <katyavov2805@gmail.com>
2024-10-04 15:07:16 +0300

    pr

--- dev/success.php
+++ dev/success.php
@@ -2,22 +2,24 @@
 header("Content-Type: text/html; charset=utf-8");
 $name = htmlspecialchars($_POST["name"]);
 $tel = htmlspecialchars($_POST["tel"]);
+$email = htmlspecialchars($_POST["email"]);
 
 $refferer = getenv('HTTP_REFERER');
 $date=date("d.m.y"); // число.месяц.год  
 $time=date("H:i"); // часы:минуты:секунды 
-$myemail = "koweb93@gmail.com";
+$myemail = "hello@silauma.agency";
 
-$tema = "Новая заявка. Кнопка (заказать звонок)";
+$tema = "Новая заявка.";
 $message_to_myemail = "
 <br><br>
 Имя: $name<br>
 Телефон: $tel<br>
+Почта: $email<br>
 
 Источник (ссылка): $refferer
 ";
 
-mail($myemail, $tema, $message_to_myemail, "From: ANDROMEDA <admin@kateweb.ru> \r\n  \r\n"."MIME-Version: 1.0\r\n"."Content-type: text/html; charset=utf-8\r\n" );
+mail($myemail, $tema, $message_to_myemail, "From: SL <admin@kateweb.ru> \r\n  \r\n"."MIME-Version: 1.0\r\n"."Content-type: text/html; charset=utf-8\r\n" );
 
 
 
